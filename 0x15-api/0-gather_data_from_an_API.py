9e98e0305749c1542d2150c65d3b97d556300966
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-05-20 20:23:56 +0000

    fixed a few issues with pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -31,8 +31,7 @@ if __name__ == "__main__":
         print(f'Employee {name} is done with tasks({completed}/{total}):')
 
         for item in todo_list:
-            if item['userId'] == ID and item['completed'] == True:
+            if item['userId'] == ID and item['completed']:
                 print(f'\t{item["title"]}')
     else:
         pass
-
