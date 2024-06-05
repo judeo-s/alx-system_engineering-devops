117f1e64a8a4cf31625ec7e2e124d3feabdd32ca
Jude Osam-Sackey <judeo-s@gmail.com>
2024-06-05 08:28:18 -0700

    made a few changes to the puppet file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,10 @@
-$settings_file='/var/www/html/wp-settings.php
+$settings_file='/var/www/html/wp-settings.php'
 file { $settings_file:
-	ensure => file,
+  ensure => file,
 }
 
 exec {'fix typo in settings config':
-	path	=> ['/bin/', '/usr/bin/', '/usr/sbin/'],
-	command	=> "sed -i s/phpp/php/g ${settings_file}",
-	require	=> File[$settings_file],
+  path    => ['/bin/', '/usr/bin/', '/usr/sbin/'],
+  command => "sed -i s/phpp/php/g ${settings_file}",
+  require => File[$settings_file],
 }
